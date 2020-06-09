e5eb71290629a033a995b85373f54930ee944ce9
Fernando Ŝajani <shayani@gmail.com>
2020-06-09 07:26:27 -0300

    Privateco kaj GPL

---
+++ pages/privateco.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="box-white">
+    <h1>Privateca politiko</h1>
+
+    <p>
+      Eventa Servo kongruas kun
+      <nuxt-link to="/gpl.txt">AGPL-3.0 licenso</nuxt-link>.
+    </p>
+
+    <p>
+      Universala Esperanto-Asocio (mallonge UEA), funkcias laŭ la nederlanda
+      juro kaj havas ĉefsidejon en Nieuwe Binnenweg 176, 3015 BJ Rotterdam,
+      Nederlando. UEA estas parto de la internacia Esperanto-movado. Kadre de
+      sia agado ĝi registras kaj prilaboras personecajn datumojn de siaj membroj
+      kaj servouzantoj. Informoj pri la datumprotektado en UEA aperas en jena
+      retejo:
+      <a href="https://uea.org/teko/datumprotekto" target="_blank"
+        >uea.org/teko/datumprotekto</a
+      >.
+    </p>
+    <p>
+      La retejo www.eventaservo.org bezonas kelkajn donitaĵojn por ebligi al
+      registritoj aldoni eventojn kaj ilin poste administri. Eventa Servo estas
+      oficiala servo de UEA kaj TEJO. Kiam vi aliĝas al la retejo, vi do
+      konsentas, ke administrantoj de UEA kaj TEJO havas aliron al viaj datumoj.
+      Validas la reguloj de datumprotektado en UEA por Eventa Servo. UEA
+      opinias, ke via privateco estas tre grava kaj delikate traktos la
+      donitaĵojn, kiujn vi havigis. Ni uzas viajn donitaĵojn nur por nia
+      administrado, por informi vin pri Eventa Servo mem kaj pri la agado de UEA
+      kaj TEJO.
+    </p>
+
+    <p>
+      Viaj donitaĵoj teorie neniam estos transdonitaj al aliaj, krom kun via
+      permeso aŭ se leĝoj devigas nin malkaŝi unuopajn datumojn, kiuj rilatas al
+      via personeco. Konforme al la Ĝenerala Datum-Protekta Regularo vi ĉiam
+      havas la rajton senpage enrigardi la donitaĵojn, kiujn ni posedas pri vi,
+      korekti ilin aŭ entute forigi ilin per simpla peto al
+      <a href="mailto:kontakto@eventaservo.org">kontakto@eventaservo.org</a>
+      (Eventa Servo) aŭ
+      <a href="mailto:datumoprotekto@co.uea.org">datumoprotekto@co.uea.org</a>
+      (UEA). Vi same povas kontakti UEA, se vi havas plendojn aŭ komentojn
+      rilate al ĝia datumprotekta regularo. Se vi havas nesolvitajn aferojn, vi
+      povas ankaŭ kontakti la respondecan nederlandan instancon. Tio eblas ĉe:
+      <a href="https://autoriteitpersoonsgegevens.nl" target="_blank"
+        >Autoriteit Persoonsgegevens</a
+      >.
+    </p>
+
+    <p>
+      La datumbazo de Eventa Servo enhavas jenajn datumojn de registritoj:
+    </p>
+
+    <ul>
+      <li>nomon</li>
+      <li>retpoŝtadreson</li>
+      <li>urbo + lando</li>
+      <li>profilbildon</li>
+      <li>organizojn kies administranto vi estas</li>
+      <li>naskiĝdaton</li>
+      <li>UEA-kodon</li>
+    </ul>
+    <br />
+    <p>
+      Ne ĉiuj kampoj estas devigaj. Vi cetere rajtas uzi kromnomon aŭ registriĝi
+      per retpoŝtadreso de asocio aŭ firmao.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'privateco',
+}
+</script>
+
+<style scoped></style>
